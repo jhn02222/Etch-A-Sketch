1819cc202785d150e866e30e709799b1a1de617a
jhn02222 <jhn02222@uga.edu>
2024-12-13 23:08:11 -0500

    new commit

--- javascript.js
+++ javascript.js
@@ -3,8 +3,8 @@ let colorOn = false;
 
 // Set container size as a fraction of the screen
 function setContainerSize() {
-    const width = window.innerWidth * 0.8; // 80% of viewport width
-    const height = window.innerHeight * 0.8; // 80% of viewport height
+    const width = Math.min(window.innerWidth * 0.8, 512, window.innerHeight); // 80% of viewport width
+    const height = Math.min(window.innerWidth * 0.8, 512, window.innerHeight); // 80% of viewport height
     container.style.width = width + "px";
     container.style.height = height + "px";
 }
